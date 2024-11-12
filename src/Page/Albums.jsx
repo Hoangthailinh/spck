ea918c33d5f40922e67f07adc65e865c49060f99
Hoangthailinh <hoangthailinh.99hl@gmail.com>
2024-11-12 21:56:31 +0700

    Linh sửa lần 2

---
+++ src/Page/Albums.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Albums() {
+  return (
+    <div>
+      <h1>Albums Page</h1>
+      <p>Discover your favorite albums here.</p>
+    </div>
+  );
+}
+
+export default Albums;
